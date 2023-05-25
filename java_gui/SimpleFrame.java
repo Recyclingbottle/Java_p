0d47dabef6f0c7e3d18781503facff88dadf52f2
Recyclingbottle <hoholeo382@gmail.com>
2023-05-25 23:31:58 +0900

    Java gui EX

---
+++ java_gui/SimpleFrame.java
@@ -0,0 +1,10 @@
+package java_gui;
+import javax.swing.JFrame;
+public class SimpleFrame {
+    public static void main(String [] args) {
+        JFrame frame;
+        frame = new JFrame("MyFrame");
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+    }
+}
