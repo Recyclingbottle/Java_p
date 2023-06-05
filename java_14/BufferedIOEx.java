40db84f59619bb712d11bf70be2c2f6b10cf21f7
Recyclingbottle <hoholeo382@gmail.com>
2023-06-05 11:42:59 +0900

    입출력 스트림과 파일 입출력 예제

---
+++ java_14/BufferedIOEx.java
@@ -0,0 +1,25 @@
+package java_14;
+
+import java.io.*;
+
+public class BufferedIOEx {
+    public static void main(String[] args) {
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedOutputStream out = new BufferedOutputStream(System.out, 5);
+
+        try {
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+            out.flush(); // 버퍼 비움
+
+            if (in != null) {
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            System.out.println("입출력 오류");
+        }
+    }
+}
